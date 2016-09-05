a839336d16a551f6c538d0feb287276038b737d7
alzanati <hosny1993.bio@gmail.com>
2016-09-06 01:53:36 +0200

    implement queue data structure

--- data-structures/Queue.cpp
+++ data-structures/Queue.cpp
@@ -21,17 +21,90 @@ public:
 template< class T >
 struct Node {
     T data;
-    
+    Node* next;
 };
 
+template< class T >
 class Queue {
 public:
-    Queue();
-    
-    ~Queue();
-    
+    Queue() : front(nullptr), back(nullptr), count(0) {}
+
+    void enqueue(T element) {
+        node temp = new Node<T>();
+        temp->data = element;
+        temp->next = nullptr;
+        if (isEmpty()) {
+            front = back = temp;
+        }
+        else {
+            temp->next = back;
+            back = temp;
+        }
+        count++;
+    }
+
+    T dequeue() {
+        node temp = back;
+        T ret;
+        if (isEmpty()) {
+            throw new QueueEmpty();
+        }
+        else {
+            if (front == back) {
+                return front->data;
+            }
+            while (true) {
+                if (temp->next == front) {
+                    ret = temp->next->data;
+                    break;
+                }
+                else {
+                    temp = temp->next;
+                }
+            }
+            delete front;
+            front = temp;
+            count--;
+            return ret;
+        }
+    }
+
+    bool isEmpty() {
+        return (count == 0) ? true : false;
+    }
+
+    int Size() {
+        return count;
+    }
+
+    void printQueueSize() {
+        println(count);
+    }
+
+    ~Queue() {}
+
+protected:
+    typedef struct Node<T> *node;
+
+private:
+    node front, back;
+    int count;
 };
 
 int main (int argc, char** argv) {
-    println("Hello world!!!");
+    Queue<int> myQueue;
+    myQueue.enqueue(2);
+    myQueue.enqueue(45);
+    myQueue.enqueue(47);
+    myQueue.enqueue(870);
+    myQueue.enqueue(234);
+    myQueue.enqueue(2364);
+    myQueue.enqueue(-1);
+    println(myQueue.dequeue());
+    println(myQueue.dequeue());
+    println(myQueue.dequeue());
+    println(myQueue.dequeue());
+    println(myQueue.dequeue());
+    println(myQueue.dequeue());
+    println(myQueue.dequeue());
 }
